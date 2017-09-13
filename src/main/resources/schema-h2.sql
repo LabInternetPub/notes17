af180a72f65822da62588756bc2e9dd50161bafd
Josep Roure Alcobé <roure@tecnocampus.cat>
2017-09-13 10:44:30 +0200

    updated user_lab table, length of varchar fields

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -2,8 +2,8 @@ DROP TABLE if EXISTS user_lab;
 CREATE TABLE user_lab
 (
   username VARCHAR (55) PRIMARY KEY,
-  name VARCHAR (55),
-  second_name VARCHAR (55),
+  name VARCHAR (255),
+  second_name VARCHAR (255),
   email VARCHAR (100)
 );
 
